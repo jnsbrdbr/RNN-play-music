c3ee9f07e7d74ad83bb06d11cd77089d918f9b44
jinus <61327864+jnsbrdbr@users.noreply.github.com>
2023-04-20 15:39:44 +0430

    initial commit

---
+++ Generate-MIDI.py
@@ -0,0 +1,35 @@
+#Generate MIDI file from a list of notes using the function below.
+def notes_to_midi(
+  notes: pd.DataFrame,
+  out_file: str, 
+  instrument_name: str,
+  velocity: int = 100,  # note loudness
+) -> pretty_midi.PrettyMIDI:
+
+  pm = pretty_midi.PrettyMIDI()
+  instrument = pretty_midi.Instrument(
+      program=pretty_midi.instrument_name_to_program(
+          instrument_name))
+
+  prev_start = 0
+  for i, note in notes.iterrows():
+    start = float(prev_start + note['step'])
+    end = float(start + note['duration'])
+    note = pretty_midi.Note(
+        velocity=velocity,
+        pitch=int(note['pitch']),
+        start=start,
+        end=end,
+    )
+    instrument.notes.append(note)
+    prev_start = start
+
+  pm.instruments.append(instrument)
+  pm.write(out_file)
+  return pm
+
+example_file = 'example.midi'
+example_pm = notes_to_midi(
+    raw_notes, out_file=example_file, instrument_name=instrument_name)
+
+display_audio(example_pm)
